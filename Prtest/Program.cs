df2c8f27db3fd6fffe90a7991f0d10dac38c288c
loadingim <68083530+loadingim@users.noreply.github.com>
2024-07-24 18:46:52 +0900

    수정
    수정

--- Prtest/Program.cs
+++ Prtest/Program.cs
@@ -9,8 +9,8 @@ namespace Prtest
     public abstract class Prtest
     {
 
-
-        // 농장 클래스를 생성<<
+        //클래스 마다 역할이 나뉘어져 있어 가독성이 좋다 <<- 가독성 관련
+        // 농장 클래스를 생성
 
         public class Farm
         {
@@ -29,7 +29,7 @@ namespace Prtest
             }
         }
 
-        //농장 클래스를 상속받아 재사용
+        //농장 클래스를 상속받아 재사용 << - 재사용성에 관련
         public class Grapeple : Farm
         {
             public Grapeple()
@@ -64,7 +64,7 @@ namespace Prtest
             }
         }
 
-        // 새로운 장비 클래스가 필요할 경우 기존 코드를 수정하지 않고 새로운 클래스를 만들어 확장 가능
+        // 새로운 장비 클래스가 필요할 경우 기존 코드를 수정하지 않고 새로운 클래스를 만들어 확장 가능 << - 유지보수 관련
 
         public static void Main()
         {
